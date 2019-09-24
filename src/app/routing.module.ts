274e2dee1d1ae82c95a05a7c1948e3a2cc6905ff
raseeth <raseeth.r@hotmail.com>
2019-09-24 17:31:38 +0100

    1 file modified

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,18 @@
 
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ROUTES } from './routes';
+
 const taskRoutes: Routes = [
-    { path: TASKROUTES.HOME, component: TaskManagerComponent,
-        children: [
-          { path: TASKROUTES.ADD, component: TaskAddComponent },
-          { path: TASKROUTES.VIEW, component: TaskViewComponent },
-          { path: TASKROUTES.EDIT, component: TaskEditComponent }
-        ],
-    }
+    { path:, component: AddTaskComponent },
+    { path:"add-task/:id", component: AddTaskComponent },
+    { path:"view-task", component: ViewTaskComponent },
+    { path:"add-project", component: ProjectComponent },
+    { path:"add-project/:id", component: ProjectComponent },
+    { path:"add-user", component: UserComponent },
+    { path:"add-user/:id", component: UserComponent },
+    { path:"", redirectTo: "view-task", pathMatch: "full" }
 ];
 
 @NgModule({
